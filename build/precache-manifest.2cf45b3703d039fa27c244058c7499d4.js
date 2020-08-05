d2cc1760f0fee64ad63ec25c40a9c125f229bab8
maxim <makkolot@gmail.com>
2020-08-05 15:59:39 +0300

    final version with dockerfile

---
+++ build/precache-manifest.2cf45b3703d039fa27c244058c7499d4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c5635e9758fc8ac2aa4db5871a135b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01cc1c86e215dfe20e15",
+    "url": "/static/css/main.74702183.chunk.css"
+  },
+  {
+    "revision": "caed6d5512b98d954907",
+    "url": "/static/js/2.79031163.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.79031163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01cc1c86e215dfe20e15",
+    "url": "/static/js/main.26534e84.chunk.js"
+  },
+  {
+    "revision": "dbaf62f757509eb3826c",
+    "url": "/static/js/runtime-main.1c95b122.js"
+  },
+  {
+    "revision": "98b4c20d99445bcee389355f148b4329",
+    "url": "/static/media/angle-down.98b4c20d.svg"
+  },
+  {
+    "revision": "2be9b3b54a6610863d32a700b61b90a6",
+    "url": "/static/media/angle-left.2be9b3b5.svg"
+  },
+  {
+    "revision": "f0db42a0988b284d02a77c0a3b60b57f",
+    "url": "/static/media/angle-right.f0db42a0.svg"
+  },
+  {
+    "revision": "dc3772ab0566a59f70e7ec21a808f6d7",
+    "url": "/static/media/angle-up.dc3772ab.svg"
+  }
+]);
